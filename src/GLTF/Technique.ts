63fae9b86f2bc7d5cc4df79e4a864fc705972414
Steve Kane <kanesteven@gmail.com>
2016-12-22 15:19:56 -0600

    more tinkering w/ Technique in GLTF

--- src/GLTF/Technique.ts
+++ src/GLTF/Technique.ts
@@ -1,11 +1,27 @@
-type Block<T> = { [ x: string ]: T }
+type Block<T> = { 
+  [ x: string ]: T
+}
 
-export type JSType
-  = number  | number[]
-  | boolean | boolean[]
-  | string  | string[]
+/*
+  here are some invariants
 
-export enum TYPE {
+  - count never present on attributes
+  - count may be present for any GLType in a uniform.  This means it's a vector
+  of that type
+  - uniforms and attributes have different semantics ( pre-defined meanings for certain values )
+  - uniformsemantics must match with certain values of GLType.  Only these combinations
+  are actually valid UniformParameter types. 
+  - attributesemantics must match certain values of GLType.  Only these combinations
+  are actually valid AttributeParameter types.
+  - uniforms may have values associated with them.  
+    Image
+    number[]
+    boolean[]
+    number
+    boolean
+*/
+
+export const enum GLType {
   BYTE = 5120,
   UNSIGNED_BYTE = 5121,
   SHORT = 5122,
@@ -29,7 +45,21 @@ export enum TYPE {
   SAMPLER_2D = 35678
 }
 
-export enum SEMANTIC {
+const enum AttributeSemanticPrefix {
+  POSITION,
+  NORMAL,
+  TEXCOORD,
+  COLOR,
+  JOINT,
+  WEIGHT
+}
+
+type AttributeSemantic = { 
+  prefix: AttributeSemanticPrefix, 
+  index: number
+}
+
+const enum UniformMat4Semantic {
   LOCAL,
   MODEL,
   VIEW,
@@ -41,37 +71,103 @@ export enum SEMANTIC {
   PROJECTIONINVERSE,
   MODELVIEWINVERSE,
   MODELVIEWPROJECTIONINVERSE,
-  MODELINVERSETRANSPOSE, // gl_float_mat3
-  MODELVIEWINVERSETRANSPOSE, //gl_float_mat3
-  VIEWPORT, //gl_float_vec4
-  JOINTMATRIX,
-  POSITION,
-  NORMAL,
-  TEXCOORD,
-  COLOR,
-  JOINT,
-  WEIGHT
+  JOINTMATRIX
 }
 
-export type Parameter = {
-  count?: number
-  node?: Node
-  type: TYPE
-  semantic?: string
-  value?: JSType
+const enum UniformMat3Semantic {
+  MODELINVERSETRANSPOSE,
+  MODELVIEWINVERSETRANSPOSE,
+  MODELVIEWPROJECTIONINVERSETRANSPOSE
 }
 
-export interface Attribute {
-  
+const enum UniformVec4Semantic {
+  VIEWPORT
 }
 
-export interface Uniform {
-
+function isSemanticMat4 ( f: Uniform ): f is SemanticMat4 {
+  return f.hasOwnProperty('semantic')
 }
+function isSemanticMat3 ( f: Uniform ): f is SemanticMat3 {
+  return f.hasOwnProperty('semantic')
+}
+function isSemanticVec4 ( f: Uniform ): f is SemanticVec4 {
+  return f.hasOwnProperty('semantic')
+}
+
+type AsNumber
+  = GLType.BYTE
+  | GLType.UNSIGNED_BYTE
+  | GLType.SHORT
+  | GLType.UNSIGNED_SHORT
+  | GLType.INT
+  | GLType.UNSIGNED_INT
+  | GLType.FLOAT
+type AsNumbers
+  = GLType.FLOAT_VEC2
+  | GLType.FLOAT_VEC3
+  | GLType.FLOAT_VEC4
+  | GLType.INT_VEC2
+  | GLType.INT_VEC3
+  | GLType.INT_VEC4
+  | GLType.FLOAT_MAT2 
+  | GLType.FLOAT_MAT3
+  | GLType.FLOAT_MAT4
+type AsBools
+  = GLType.BOOL_VEC2
+  | GLType.BOOL_VEC3
+  | GLType.BOOL_VEC4
+
+type SemanticMat4 = { type: GLType.FLOAT_MAT4, semantic: UniformMat4Semantic, value?: number[] }
+type SemanticMat3 = { type: GLType.FLOAT_MAT3, semantic: UniformMat3Semantic, value?: number[] }
+type SemanticVec4 = { type: GLType.FLOAT_VEC4, semantic: UniformVec4Semantic, value?: number[] }
+
+export type Uniform
+  = SemanticMat4
+  | SemanticMat3
+  | SemanticVec4
+  | { type: AsNumber, count?: number, value?: number }
+  | { type: AsNumbers, count?: number, value?: number[] }
+  | { type: GLType.BOOL, count?: number, value?: boolean }
+  | { type: AsBools, count?: number, value?: boolean[] }
+  | { type: GLType.SAMPLER_2D, value?: HTMLImageElement }
+
+function test ( f: Uniform ) {
+  switch ( f.type ) {
+    case GLType.FLOAT_MAT4: return isSemanticMat4(f) ? console.log(f.semantic) : f.count
+    case GLType.FLOAT_MAT3: return isSemanticMat3(f) ? console.log(f.semantic) : f.count
+    case GLType.FLOAT_VEC4: return isSemanticVec4(f) ? console.log(f.semantic) : f.count
+    case GLType.SAMPLER_2D: return console.log(f.type)
+    case GLType.FLOAT: return console.log(f.type)
+    case GLType.FLOAT_MAT2: return console.log(f.type)
+    case GLType.BYTE: return console.log(f.type)
+    case GLType.FLOAT_VEC2: return console.log(f.type)
+    case GLType.FLOAT_VEC3: return console.log(f.type)
+    case GLType.UNSIGNED_BYTE: return console.log(f.type)
+    case GLType.SHORT: return console.log(f.type)
+    case GLType.UNSIGNED_SHORT: return console.log(f.type)
+    case GLType.INT: return console.log(f.type)
+    case GLType.UNSIGNED_INT: return console.log(f.type)
+    case GLType.INT_VEC2: return console.log(f.type)
+    case GLType.INT_VEC3: return console.log(f.type)
+    case GLType.INT_VEC4: return console.log(f.type)
+    case GLType.BOOL: return console.log(f.type)
+    case GLType.BOOL_VEC2: return console.log(f.type)
+    case GLType.BOOL_VEC3: return console.log(f.type)
+    case GLType.BOOL_VEC4: return console.log(f.type)
+    default: const n: never = f
+             return n
+  }
+}
+
+// TODO: these are just examples
+test({ type: GLType.FLOAT_MAT4, semantic: UniformMat4Semantic.JOINTMATRIX })
+test({ type: GLType.FLOAT_MAT3 })
+test({ type: GLType.SAMPLER_2D })
+
+export interface Attribute {}
 
 export interface Technique {
   program: WebGLProgram
-  parameters?: Block<Parameter>
   attributes?: Block<Attribute>
   uniforms?: Block<Uniform>
 }
